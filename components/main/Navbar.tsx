88dd3e8c4736babbec40d35b8929914ba434c083
Amit Singh <64282124+dev-amit-singh@users.noreply.github.com>
2025-08-25 14:18:48 +0530

    Update Navbar.tsx

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
 
             <div className='w-[500px] h-full flex flex-row items-center justify-between md:mr-20'>
                 <div className='nav flex items-center justify-between h-auto w-full border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200'>
-                    <a href="#about-me" className='cursor-pointer'>About me</a>
+                    <a href="#about" className='cursor-pointer'>About me</a>
                     <a href="#skills" className='cursor-pointer'>Skills</a>
                     <a href="#projects" className='cursor-pointer'>Projects</a>
                 </div>
